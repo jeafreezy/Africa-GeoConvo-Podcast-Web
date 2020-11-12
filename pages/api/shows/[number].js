46c58fe2b4da42dd64acb8abc912b69eafe90b69
jeafreezy <jolaiyaemmanuel@gmail.com>
2020-11-12 13:31:30 +0100

    refactoring

--- pages/api/shows/[number].js
+++ pages/api/shows/[number].js
@@ -1,9 +1,9 @@
-import Cors from 'cors';
-import { getAllShows } from '../../../utils/ReadShowFiles';
+
+import {  getShows } from '../../../utils/ReadShowFiles';
 
 export default async function showByNumber(req, res) {
 
-  const show = await getAllShow(req.query.number);
+  const show = await getShows(req.query.number);
   if (show) {
     res.json(show);
     return;
