881ad7d2f4db7503af44e4a3cd485a643630fe36
jeafreezy <jolaiyaemmanuel@gmail.com>
2020-11-12 13:54:33 +0100

    refactoring

--- utils/ReadShowFiles.js
+++ utils/ReadShowFiles.js
@@ -18,7 +18,7 @@ let shows;
 const loadShows = async()=>{
 
 
-   const showsDirectory = path.join(path.dirname(process.cwd()),'shows');
+   const showsDirectory = path.join((process.cwd(),'/shows');
 
    const files = (await readAFolder(showsDirectory)).filter(file => file.endsWith('.md'))
 
