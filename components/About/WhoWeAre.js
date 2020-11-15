a4c7662b898d7522591742a83695caf69b1bc827
jeafreezy <jolaiyaemmanuel@gmail.com>
2020-11-15 11:19:20 +0100

    changed dark more images

--- components/About/WhoWeAre.js
+++ components/About/WhoWeAre.js
@@ -1,39 +1,86 @@
 import React from 'react';
 import {TitleStyles} from '../utilsStyles';
 import { WhoWeAreStyle } from '../Styles/About';
-
+import DarkModeStatus from '../Header/DarkModeStatus';
 
 function WhoWeAre(){
 
-    return(
-        <>
-                <TitleStyles>Who We Are</TitleStyles>
-                    
-                <WhoWeAreStyle>
-                    <div className='container'>
+    const status =DarkModeStatus()
 
-                        <div className='about-info'>
-                            <p>
-                                    I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a account of the system.
+    if(status === 'Light Mode'){
 
-                                    Nunc sit amet neque imperdiet, maximus massa id, vulputate magna. Vestibulum eget mi blandit, volutpat diam ut, ullamcorper eros In sed dolor eu velit pretium lacinia. Nullam interdum tristique lorem sit amet facilisis. lobortis scelerisque. Suspendisse a quam sed libero cursus. molestie nec vitae metus.
-
-                                    Sit amet, consectetur adipiscing elit. Quisque imperdiet eros leo, eget consequat orci viverra nec. Suspendisse pellentesque sem metus, et mollis purus auctor in. Suspendisse congue quam eros, vel cursus ante semper id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            </p>
+        return(
+            <>
+                    <TitleStyles>Who We Are</TitleStyles>
+                        
+                    <WhoWeAreStyle>
+                        <div className='container'>
+    
+                            <div className='about-info'>
+                                <p>
+                                        I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a account of the system.
+    
+                                        Nunc sit amet neque imperdiet, maximus massa id, vulputate magna. Vestibulum eget mi blandit, volutpat diam ut, ullamcorper eros In sed dolor eu velit pretium lacinia. Nullam interdum tristique lorem sit amet facilisis. lobortis scelerisque. Suspendisse a quam sed libero cursus. molestie nec vitae metus.
+    
+                                        Sit amet, consectetur adipiscing elit. Quisque imperdiet eros leo, eget consequat orci viverra nec. Suspendisse pellentesque sem metus, et mollis purus auctor in. Suspendisse congue quam eros, vel cursus ante semper id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                </p>
+                            </div>
+    
+                            <div className='about-image'>
+                            
+                                <img src="/static/assets/logo-blue.png"
+                                    width="150"
+                                    height="45"
+                                    alt='Africa GeoConvo logo'
+                                    title='Africa GeoConvo'
+                                    loading="lazy"/>
+                                
+                            </div>
+    
                         </div>
+                    </WhoWeAreStyle>
+    
+    
+            </>
+        )
+    }else{
 
-                        <div className='about-image'>
-
-                            <img src='/static/assets/logo-blue.png' alt='Africa GeoConvo small logo' title= 'Africa GeoConvo'></img>
-
+        return(
+            <>
+                    <TitleStyles>Who We Are</TitleStyles>
+                        
+                    <WhoWeAreStyle>
+                        <div className='container'>
+    
+                            <div className='about-info'>
+                                <p>
+                                        I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a account of the system.
+    
+                                        Nunc sit amet neque imperdiet, maximus massa id, vulputate magna. Vestibulum eget mi blandit, volutpat diam ut, ullamcorper eros In sed dolor eu velit pretium lacinia. Nullam interdum tristique lorem sit amet facilisis. lobortis scelerisque. Suspendisse a quam sed libero cursus. molestie nec vitae metus.
+    
+                                        Sit amet, consectetur adipiscing elit. Quisque imperdiet eros leo, eget consequat orci viverra nec. Suspendisse pellentesque sem metus, et mollis purus auctor in. Suspendisse congue quam eros, vel cursus ante semper id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                </p>
+                            </div>
+    
+                            <div className='about-image'>
+            
+                                <img src="/static/assets/logo-white.png"
+                                    width="150"
+                                    height="45"
+                                    alt='Africa GeoConvo logo'
+                                    title='Africa GeoConvo'
+                                    loading="lazy"/>
+                                
+                            </div>
+    
                         </div>
-
-                    </div>
-                </WhoWeAreStyle>
-
-
-        </>
-    )
+                    </WhoWeAreStyle>
+    
+    
+            </>
+        )
+    }
+    
 }
 
 export default WhoWeAre;
--- components/Header/Navigation.js
+++ components/Header/Navigation.js
@@ -3,37 +3,67 @@ import Burger from './Burger';
 import { StyledNav } from '../Styles/Header';
 import Link from 'next/link';
 import DarkModeToggle from './DarkModeToggle';
-import useDarkMode from 'use-dark-mode';
+import DarkModeStatus from './DarkModeStatus';
 
 function Navigation(){
-    const darkMode = useDarkMode(false);
-    return(
-        <>
-          
-            <StyledNav>
-            <div>
-                <DarkModeToggle />
-            </div>
-           
-                <div className='logo'>
-                    <Link href='/'>
-                    <picture >
-                            <img src="/static/assets/logo.png"
-                                width="150"
-                                height="45"
-                                alt='Africa GeoConvo logo'
-                                title='Africa GeoConvo'
-                                loading="lazy"/>
-                    </picture>
-                    </Link>
-                </div>
+
+    const status = DarkModeStatus()
+    if(status === 'Light Mode'){
+
+        return(
+            <>
+              
+                <StyledNav>
                 <div>
-                    <Burger />
+                    <DarkModeToggle />
                 </div>
                
-            </StyledNav>
-        </>   
-    )
+                    <div className='logo'>
+                        <Link href='/'>
+                                <img src="/static/assets/logo.png"
+                                    width="150"
+                                    height="45"
+                                    alt='Africa GeoConvo logo'
+                                    title='Africa GeoConvo'
+                                    loading="lazy"/>
+                        </Link>
+                    </div>
+                    <div>
+                        <Burger />
+                    </div>
+                   
+                </StyledNav>
+            </>   
+        )
+    }else{
+
+        return(
+            <>
+              
+                <StyledNav>
+                <div>
+                    <DarkModeToggle />
+                </div>
+               
+                    <div className='logo'>
+                        <Link href='/'>
+                                <img src="/static/assets/white.png"
+                                    width="150"
+                                    height="45"
+                                    alt='Africa GeoConvo logo'
+                                    title='Africa GeoConvo'
+                                    loading="lazy"/>
+                        </Link>
+                    </div>
+                    <div>
+                        <Burger />
+                    </div>
+                   
+                </StyledNav>
+            </>   
+        )
+    }
+    
 }
 
 export default Navigation;
