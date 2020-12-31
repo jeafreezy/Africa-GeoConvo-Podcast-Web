d1220f1efea2c357776169d783ba2d8f7e1c9e22
jeafreezy <jolaiyaemmanuel@gmail.com>
2020-12-31 02:28:13 +0100

    fixed typos

--- components/Subscribe/Subscribe.js
+++ components/Subscribe/Subscribe.js
@@ -29,7 +29,7 @@ function Subscribe(){
                                 </div>
                                 <div>
                                     <a href='https://www.google.com/podcasts?feed=aHR0cHM6Ly9mZWVkLnBvZGJlYW4uY29tL2FmcmljYWdlb2NvbnZvL2ZlZWQueG1s' target='_blank' rel='noopener noreferer'><img src='/static/assets/google-podcasts.png' alt='Google podcast icon'></img></a>
-                                    <p>Google podcast</p>
+                                    <p>Google podcasts</p>
                                 </div>
                                 <div>
                                     <a href='https://feed.podbean.com/africageoconvo/feed.xml' target='_blank' rel="noopener noreferer"> <img src='/static/assets/rss.png' alt='Rssfeeds podcast icon'></img></a>
